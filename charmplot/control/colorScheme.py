8885f6e52403886d0e13e3b1dbbf096fd0ab4cb5
Miha Muskinja <miha.muskinja@cern.ch>
2020-02-25 21:54:59 -0800

    new color scheme

--- charmplot/control/colorScheme.py
+++ charmplot/control/colorScheme.py
@@ -12,14 +12,15 @@ def RGB(string):
     return tuple(int(string[i + 1:i + 3], 16) / 255. for i in (0, 2, 4))
 
 
-def berkeley_wjets():
-    logger.info("Setting color scheme to 'berkeley_wjets'")
+# https://www.colourlovers.com/palette/1930/cheer_up_emo_kid
+def cheer_up_emo_kid():
+    logger.info("Setting color scheme to 'cheer_up_emo_kid'")
     scheme = [ROOT.TColor(10000 + i, *RGB(c)) for i, c in enumerate([
-        "#93c4d2",
-        "#00429d",
-        "#ffa59e",
-        "#93003a",
-        "#ffffbf",
+        "#4ECDC4",
+        "#556270",
+        "#FF6B6B",
+        "#C44D58",
+        "#C7F464",
         "#c9c9c9",
     ])]
     return scheme
